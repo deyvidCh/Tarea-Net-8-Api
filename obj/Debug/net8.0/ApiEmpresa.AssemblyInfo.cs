dd9f06387bc199ab5ae601b41e16232c9f071f59
deyvidCh <deyvidchinchilla2@gmail.com>
2024-10-08 23:00:51 -0600

    Ejecutando pruebas Con conexion SQL

--- obj/Debug/net8.0/ApiEmpresa.AssemblyInfo.cs
+++ obj/Debug/net8.0/ApiEmpresa.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiEmpresa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5405c28dc7ca403872637ef867bdf8df516e85bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiEmpresa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiEmpresa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
