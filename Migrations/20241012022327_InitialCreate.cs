1ed0ac3c39dcb8eeb001fd1ef90d6c15afe72bdc
deyvidCh <deyvidchinchilla2@gmail.com>
2024-10-11 20:43:44 -0600

    Cambiando Server para BD

---
+++ Migrations/20241012022327_InitialCreate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace ApiEmpresa.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    id_cliente = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    nit = table.Column<string>(type: "longtext", nullable: true),
+                    nombres = table.Column<string>(type: "longtext", nullable: true),
+                    apellidos = table.Column<string>(type: "longtext", nullable: true),
+                    direccion = table.Column<string>(type: "longtext", nullable: true),
+                    telefono = table.Column<string>(type: "longtext", nullable: true),
+                    fecha_nacimiento = table.Column<DateTime>(type: "datetime(6)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.id_cliente);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+        }
+    }
+}
--- obj/Debug/net8.0/ApiEmpresa.AssemblyInfo.cs
+++ obj/Debug/net8.0/ApiEmpresa.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiEmpresa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ccce7c31b9c1ae41e1f8c64fafb6512be8d43f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29bc708c92cace7c680da159aff21f4a4b4a7499")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiEmpresa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiEmpresa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
